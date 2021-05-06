dc86c5ae7ef8bb4d6c0208922c76c495f7eba4e0
Beka Abuladze <35916941+beqaabu@users.noreply.github.com>
2021-05-06 12:50:17 +0400

    Update Apps.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Address from './Components/Address.js';
-import SelectCoins from './Components/SelectCoins.js';
 import SelectType from './Components/SelectType';
 
 function App() {
   return (
     <div className="App">
       <SelectType />
-      <SelectCoins />
-      <SelectCoins />
-      <Address />
     </div>
   );
 }
