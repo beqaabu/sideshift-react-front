9542ba1e4b2419698554a495ab257ba65108c6ce
Beka Abuladze <35916941+beqaabu@users.noreply.github.com>
2021-05-06 12:47:23 +0400

    dynamically change exchange types

--- src/Components/SelectType.js
+++ src/Components/SelectType.js
@@ -1,29 +1,64 @@
 import React from 'react';
-let x = '';
+import SelectCoins from './SelectCoins';
+import EnterAmount from './EnterAmount';
+import Address from './Address.js';
 
 class SelectType extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            value: ''
+            type: ''
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event){
-        this.setState({value: event.target.value});
-        //type = event.target.value;
+        this.setState({type: event.target.value});
     }
 
     render(){
+        if(this.state.type !== ''){
+            return(
+                <div>
+                    {TypeSelection(this)}
+                </div>
+            )
+        }
+        else {
+            return(
+                <div className="type">
+                    <p>Select the Type of the Exchange</p>
+                    <select value={this.state.type} onChange={this.handleChange}>
+                        <option value=''>Select</option>
+                        <option value="fixed">Fixed</option>
+                        <option value="variable">Variable</option>
+                    </select>
+                    <p>{TypeSelection(this)}</p>
+
+                    
+                </div>
+            )
+        }
+    }
+}
+
+function TypeSelection(props){
+    if(props.state.type === 'fixed'){
         return(
-            <div className="type">
-                <select value={this.state.value} onChange={this.handleChange}>
-                    <option value=''>Select</option>
-                    <option value="fixed">Fixed</option>
-                    <option value="variable">Variable</option>
-                </select>
-                {x = this.state.value}
+            <div>
+                <p>Fixed exchange type selected</p>
+                <SelectCoins />
+                <SelectCoins />
+                <EnterAmount />
+            </div>
+        )
+    } else if(props.state.type === 'variable'){
+        return(
+            <div>
+                <p>Variable exchange type selected</p>
+                <SelectCoins />
+                <SelectCoins />
+                <Address />
             </div>
         )
     }
